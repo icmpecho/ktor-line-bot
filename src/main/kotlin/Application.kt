fd6ad068d91fb2c2a73884c0cef0948d935d3cf7
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2020-06-21 23:33:39 +0700

    Split ktor module out of application

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,1 @@
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
--- src/main/kotlin/com/icmpecho/linebot/Module.kt
+++ src/main/kotlin/com/icmpecho/linebot/Module.kt
@@ -1,17 +1,17 @@
-package com.icmpecho
+package com.icmpecho.linebot
 
 import com.icmpecho.linebot.event.model.WebHookRequest
-import io.ktor.application.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.request.receive
-import io.ktor.response.*
+import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.serialization.DefaultJsonConfiguration
 import io.ktor.serialization.json
-
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 fun Application.module(testing: Boolean = false) {
@@ -36,7 +36,3 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 }
-
-
-
-
