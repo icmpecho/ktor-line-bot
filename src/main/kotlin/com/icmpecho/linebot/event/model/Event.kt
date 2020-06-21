a689ef0784e0f9a5b3d63a40fdfaee65881b6763
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2020-06-21 19:37:22 +0700

    Remove event type and change into event subclasses

--- src/main/kotlin/com/icmpecho/linebot/event/model/Event.kt
+++ src/main/kotlin/com/icmpecho/linebot/event/model/Event.kt
@@ -4,11 +4,6 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class Type {
-    @SerialName("message") MESSAGE,
-}
-
-@Serializable
 enum class Mode {
     @SerialName("active") ACTIVE,
     @SerialName("standby") STANDBY,
@@ -17,18 +12,23 @@ enum class Mode {
 
 @Serializable
 sealed class Source {
-    @SerialName("user")
+
     @Serializable
+    @SerialName("user")
     data class User(
         @SerialName("userId") val userId: String
     ):Source()
 }
 
 @Serializable
-data class Event(
-    @SerialName("replyToken") val replyToken: String,
-    @SerialName("type") val type: Type,
-    @SerialName("mode") val mode: Mode = Mode.UNKNOWN,
-    @SerialName("timestamp") val timestamp: Long,
-    @SerialName("source") val source: Source
-)
+sealed class Event {
+
+    @Serializable
+    @SerialName("message")
+    data class MessageEvent(
+        @SerialName("replyToken") val replyToken: String,
+        @SerialName("mode") val mode: Mode = Mode.UNKNOWN,
+        @SerialName("timestamp") val timestamp: Long,
+        @SerialName("source") val source: Source
+    ):Event()
+}
